1f7a200d2cfab950732fcb18d4484f9b404a3616
Mdiab-hub <Mdiab-hub>
2020-06-14 15:55:05 -0400

    datatype refresher complete

---
+++ Mod1/week-3 /w3d3/homework/answers.js
@@ -0,0 +1,59 @@
+//e.g. A computer that can be either on or off. 
+//=> datatype: boolean 
+//=> data structure example: const computer = true; 
+
+//A light switch that can be either on or off.
+datatype: boolean
+const lightSwitch = true
+
+//A user's email address.
+datatype: string
+const emailAddress = ['jon.doe@yahoo.com']
+
+//A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+datatype: object
+const spaceship = {
+
+desription: ['hull', 'laser blasters', 'tractor beam', 'warp drive']
+
+}
+
+//A list of student names from our class.
+datatype: Array
+const names = ['jessica', 'albert', 'ryan']
+
+
+//A list of student names from our class, each with a location.
+dataype: object
+const studentName = {
+    'jessica'{
+    location: 'California',
+    },
+
+  'robert'{
+      location: 'Maine',
+  },
+
+  'william'{
+      location: 'Ohio'
+  }
+}
+
+//A list of student names from our class, each with a location and each with a list of favorite tv shows.
+dataype: object
+const studentName = {
+    'jessica'{
+    location: 'California',
+    tvShows: ['the office', 'parks and recreation']
+    },
+
+  'robert'{
+      location: 'Maine',
+      tvShows: ['the office', 'parks and recreation']
+  },
+
+  'william'{
+      location: 'Ohio',
+      tvShows: ['the office', 'parks and recreation']
+  }
+}
