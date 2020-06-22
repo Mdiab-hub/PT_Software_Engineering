ccd5915fb3b51de92190ffd7c0ad1a2224158ee4
Mdiab-hub <Mdiab-hub>
2020-06-22 19:41:31 -0400

    week1 HW

--- Mod1/week-1/w1d2/first_js_homework .js
+++ Mod1/week-1/w1d2/first_js_homework .js
@@ -1,3 +1,2 @@
-//The difference between interpolation and concatentation
 // DRY code means to use an overarching logic, that the rest of the code can follow. It helps with using the same logic without having to retype it again. You can use Intellij IDEA is one of the tools that can help you help you in writing DRY code.
-// var you can assign and redeclared withing its scope; let variables can be reassigned but not redeclared; const can neither be reassigned or redeclared
+// var you can assign and redeclared withing its scope; let variables can be reassigned but not redeclared; const can neither be 
