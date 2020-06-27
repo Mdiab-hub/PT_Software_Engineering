ac24b77bfb5300c089f19a5bc42e90394f2d0f29
Mdiab-hub <Mdiab-hub>
2020-06-27 00:50:43 -0400

    created hamster class

--- Mod1/week-5/w5d1/homework/objects.js
+++ Mod1/week-5/w5d1/homework/objects.js
@@ -30,7 +30,7 @@
 
 
 
-class Surgeon {
+/*class Surgeon {
     constructor(name, department) {
       this._name = name;
       this._department = department;
@@ -52,17 +52,20 @@ class Surgeon {
   
   const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
   //console.log(surgeonCurry)
-  const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
+  const surgeonDurant = new Surgeon('Durant', 'Orthopedics');*/
+
 /////////////////////////
 //Creating Classes///////
 ////////////////////////
 
 class Hamster {
-    constructor(owner, name, price)
-    owner = ''
-    name = ''
-    price = 15
-}
+    constructor(owner, name, price){
+    this._owner = owner ; ''
+    this._name = name
+    this._price = price
+    //console.log(Hamster)
+    }
+
 wheelRun() {
     console.log("squeak squeak")
 }
@@ -72,4 +75,69 @@ eatFood(){
 getPrice(){
     return this._price
 }
-console.log(Hamster)
+//console.log(Hamster)
+
+}
+
+/////////////
+///Person///
+////////////
+
+class Person {
+    constructor(name, age, height, weight, mood, hamsters, bankAccount){
+        this._name = name
+        this._age = age
+        this._height = height
+        this._weight = weight
+        this._mood = mood
+        this._hamster = []
+        this._bankAccount = bankAccount
+    }
+    getName(){
+        return this._name
+    }
+    getAge(){
+        return this._age
+    }
+    getWeight(){
+        return this._height
+    }
+    greet(){
+        console.log("Jason")
+    }
+    eat(){
+    this._weight++;
+    this._mood++      
+    }
+    exercise(){
+        this._weight--
+    }
+    ageUp(){
+        this._age++
+        this._height++
+        this._weight++
+        this._mood--
+        this._bankAccount += 10
+    }
+    //buyHamster(hamster) - push the hamster object onto the hamster array, increment mood by 10, decrement bankAccount by the value of the hamster (hint: use getPrice())
+    buyHamster(hamster){
+    this._hamster.push(Hamster)
+    this._mood += 10
+
+    //decrement bankAccount by the value of the hamster (hint: use getPrice())
+    getPrice(); {
+    this._bankAccount-- //(hamster) //I have no idea.
+    }
+    
+    }
+}
+
+const timmy = new Person ('timmy', 5, 5, 5)
+console.log(timmy)
+
+const hamster = new Hamster ('Timmy', 'Gus',)
+// constructor(owner, name, price){
+//     this._owner = owner ; ''
+//     this._name = name
+//     this._price = price
+
