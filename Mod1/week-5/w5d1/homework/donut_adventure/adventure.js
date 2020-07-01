6b1890a49c27c39cef2b55100e5945f06e39d158
Mdiab-hub <Mdiab-hub>
2020-07-01 01:12:06 -0400

    current week HW

--- Mod1/week-5/w5d1/homework/donut_adventure/adventure.js
+++ Mod1/week-5/w5d1/homework/donut_adventure/adventure.js
@@ -3,37 +3,122 @@ class Hero {
     this.name = name
     this.health = 100
     this.weapon = { sprinkleSpray: 5, sugarShock: 10 }
-    this.catchPhrase = ['i\'m fresher than day old pizza', 
-    'you can\'t count my calories']
+    this.catchPhrase = ['i\'m fresher than day old pizza', 'you can\'t count my calories']
+}
+talkSass(){
+    console.log(this.catchPhrase[Math.floor(Math.random() * this.catchPhrase.length)])
+} 
+announceHealth(){
+    console.log(this.health)
+}
+fight(_Enemy){ //<----------- Outputs: TypeError: Cannot read property 'healthy' of undefined
+    console.log('i\'m ready to rumble')
+        let random = Math.round(Math.random())
+    if (random == 1){
+        this.health -= this.weapon.sugarShock 
+        console.log(`${this.name} has used sugar shock`)
+    } else {
+        this.health -= this.weapon.sprinkleSpray;
+        console.log(`${this.name} health is ` + this.health)
     }
-
-    talkSass(){
-        console.log(Math.random(this.catchPhrase))
-    } 
-    announceHealth(){
-        console.log(this.health)
-    }
-    fight(){
-        console.log('i\'m ready to rumble')
+        console.log(`${this.name} health is ` + this.health)
+        console.log(`${this.name} health is `+ this.health)
     }
 }
 
-const dougieTheDonut = new Hero ()
+
+
+const dougietheDonut = new Hero ("Dougie the Donut")
 
 class Enemy {
     constructor(name){
-        this.name = name
-        this.health = 100
-        this.weapons = { pepperoniStars: 5, cheeseGrease: 10 }
-        this.catchPhrase = ['i\'m youtube famous', 'i\'m more dangerous than an uncovered sewer']
-    }
-    talkSmack() {
-        console.log(Math.random(this.catchPhrase))
-    }
-    announceHealth(){
-        console.log(this.health)
-    }
-    fight(){
-        console.log('i\'m gonna flatten you like a slice of pepperoni!')
-    }
+    this.name = name
+    this.health = 100
+    this.weapon = { pepperoniStars: 5, cheeseGrease: 10 }
+    this.catchPhrase = ['i\'m fresher than day old pizza', 'you can\'t count my calories']
 }
+talkSmack(){
+    console.log(this.catchPhrase[Math.floor(Math.random() * this.catchPhrase.length)])
+} 
+announceHealth(){
+    console.log(this.health)
+}
+
+fight(_Hero){ //<-----TypeError: Cannot read property 'healthy' of undefined
+        console.log('i\'m ready to rumble')
+
+        let random = Math.round(Math.random())
+
+        if (random == 1){
+            this.health -= this.weapon.pepperoniStars 
+            console.log(`${this.name} has used sugar shock`)
+        } else {
+            this.healthy -= this.weapon.cheeseGrease;
+            console.log(`${this.name} health is ` + this.health)
+        }
+            console.log(`${this.name} health is ` + this.health)
+            console.log(`${this.name} health is `+ this.health)
+        }
+    }
+
+
+const pizzaRat = new Hero ("Pizza Rat")
+
+const dougieTalkSass = new Hero()
+dougieTalkSass.talkSass()
+
+const pizzaRatTalkSmack = new Enemy()
+pizzaRatTalkSmack.talkSmack()
+
+const dougieAnnounceHealth = new Hero()
+dougieAnnounceHealth.announceHealth()
+
+const pizzaAnnounceHealth = new Enemy()
+pizzaAnnounceHealth.announceHealth()
+
+const dougieWeapon = new Hero()
+dougieWeapon.fight()
+
+const pizzaRatWeapon = new Enemy()
+pizzaRatWeapon.fight()
+
+/*class Enemy {
+    constructor(
+        name,
+        health = 100,
+        weapon = {
+            pepperoniStars: 5,
+            cheeseGrease: 10
+        },
+        catchPhrase = ['Im youtube famous', 'Im more dangerous than an uncovered sewer']
+    )
+    {
+        this.name = name
+        this.health = health
+        this.weapon = weapon
+        this.catchPhrase = catchPhrase
+    }
+    talkSmack(){
+            console.log(this.catchPhrase[Math.floor(Math.random() * this.catchPhrase.length)])
+        } 
+        announceHealth(){
+            console.log(this.health)
+        }
+      
+            fight(enemy){
+                console.log('i\'m ready to rumble')
+        
+                let random = Math.round(Math.random())
+        
+                if (random == 1){
+                    enemy.health -= this.weapon.pepperoniStars 
+                    console.log(`${this.name} has used sugar shock`)
+                } else {
+                    enemy.healthy -= this.weapon.cheeseGrease;
+                    console.log(`${this.name} health is ` + this.health)
+                }
+                    console.log(`${this.name} health is ` + this.health)
+                    console.log(`${enemy.name} health is `+ enemy.health)
+                }
+    
+}*/
