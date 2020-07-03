916ee78e4e3da497db204c420eeb7308855ca3c7
Mdiab-hub <Mdiab-hub>
2020-07-03 00:20:36 -0400

    currrent week HW

--- Mod1/week-5/w5d1/homework/objects.js
+++ Mod1/week-5/w5d1/homework/objects.js
@@ -1,66 +1,10 @@
-// class Dog {
-//     constructor(name) {
-//       this._name = name;
-//       this._behavior = 0;
-//     }
-  
-//     get name() { //<--- This is a function
-//       return this._name;
-//     }
-//     get behavior() {
-//       return this._behavior;
-//     }   
-  
-//     incrementBehavior() {
-//       this._behavior ++;
-//     }
-//   }
-  
-//   const halley = new Dog('Halley');
-
-//   console.log(halley.name); // Print name value to console
-  
-//   console.log(halley.behavior); // Print behavior value to console
-  
-//   halley.incrementBehavior(); // Add one to behavior
-  
-//   console.log(halley.name); // Print name value to console
-  
-//   console.log(halley.behavior); // Print behavior value to console
-
-
-
-/*class Surgeon {
-    constructor(name, department) {
-      this._name = name;
-      this._department = department;
-      this._remainingVacationDays = 20;
-    }
-    get name(){
-       return this._name
-     }
-     get department(){
-       return this._department
-     }
-     get remainingVacationDays () {
-       return this._remainingVacationDays
-     }
-     takeVacationDays(daysOff = 5){
-       return this._remainingVacationDays - daysOff
-     }
-  }
-  
-  const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
-  //console.log(surgeonCurry)
-  const surgeonDurant = new Surgeon('Durant', 'Orthopedics');*/
-
 /////////////////////////
 //Creating Classes///////
 ////////////////////////
 
 class Hamster {
     constructor(owner, name, price){
-    this._owner = owner ; ''
+    this._owner = owner 
     this._name = name
     this._price = price
     //console.log(Hamster)
@@ -69,22 +13,24 @@ class Hamster {
 wheelRun() {
     console.log("squeak squeak")
 }
-eatFood(){
+eatFood() {
     console.log("nibble nibble")
 }
-getPrice(){
-    return this._price
-}
+// getPrice(){
+//     return this._price
+// }
 //console.log(Hamster)
-
 }
+
+//console.log(newHamster)
 
 /////////////
 ///Person///
 ////////////
 
 class Person {
-    constructor(name, age, height, weight, mood, hamsters, bankAccount){
+    hamster = []
+    constructor(name, age, height, weight, mood, _hamster, bankAccount) {
         this._name = name
         this._age = age
         this._height = height
@@ -119,25 +65,66 @@ class Person {
         this._mood--
         this._bankAccount += 10
     }
-    //buyHamster(hamster) - push the hamster object onto the hamster array, increment mood by 10, decrement bankAccount by the value of the hamster (hint: use getPrice())
-    buyHamster(hamster){
-    this._hamster.push(Hamster)
+
+    //buyHamster(hamster) - 
+    //push the hamster object onto the hamster array, 
+    //increment mood by 10, 
+    //decrement bankAccount by the value of the hamster (hint: use getPrice())
+    
+
+    buyHamster(hamster) {
+    this._hamster.push(newHamster)
     this._mood += 10
+    }
 
     //decrement bankAccount by the value of the hamster (hint: use getPrice())
-    getPrice(); {
-    this._bankAccount-- //(hamster) //I have no idea.
+    getPrice() {
+        this._bankAccount-- //(hamster) //I have no idea.
     }
     
     }
-}
+    
 
+
+
+
+//const punie = this._hamster.push(newHamster)
+//console.log(punie)
+
+const newHamster = new Hamster ('Larry','Punie', 20)
 const timmy = new Person ('timmy', 5, 5, 5)
-console.log(timmy)
 
-const hamster = new Hamster ('Timmy', 'Gus',)
+console.log(timmy.buyHamster(newHamster))
+
+
+
+// const hamster = new Hamster ('Timmy', 'Gus',)
 // constructor(owner, name, price){
 //     this._owner = owner ; ''
 //     this._name = name
 //     this._price = price
+
+//Instantiate a new Person named Timmy
+const Timmy = new Person("Timmy");
+//Age Timmy five years
+console.log(Timmy);
+Timmy.ageUp(5);
+// At this point Timmy's a little bummed.  As a precocious child, he feels he's "seen it all" already.  Have him eat five times.
+Timmy.eat(5);
+// Now Timmy's a little heavier than he wants to be.  Kindergarten's coming up and he wants to look good.  Have him exercise five times
+Timmy.exercise(5);
+// Age Timmy 9 years
+Timmy.ageUp(9);
+console.log(Timmy);
+// Create a hamster named "Gus"
+// Set Gus's owner to the string "Timmy"
+const Gus = new Hamster(Timmy, "Gus");
+// Have Timmy "buy" Gus
+// Timmy.buyHamster("Gus");
+// Age Timmy 15 years
+Timmy.ageUp(15);
+//Have Timmy eat twice
+Timmy.eat(2);
+// Have Timmy exercise twice
+Timmy.exercise(2);
 
