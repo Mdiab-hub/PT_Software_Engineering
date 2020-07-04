e527368466067860390062ba125a560cf44066ef
Mdiab-hub <Mdiab-hub>
2020-07-03 20:12:08 -0400

    w5d3_lab

---
+++ Mod1/week-5/w5d3/vanilla_dom_practice/dom_practice.js
@@ -0,0 +1,24 @@
+console.log("hello")
+// let x = document.children[0].children[1].children[4].id
+let y = document.children[0].children[1].children[5]
+console.log(y)
+let h2 = document.children[0].children[1].children[0]
+console.log(h2)
+
+let z = document.children[0].children[1].children[0]//.innerText
+//document.children[0].children[1].children[0].innerText = "!!!"
+z.innerText = "!!!"
+console.log(z.innerText)
+//document.getElementById("!!!").innerText
+
+let work = document.children[0].children[1].children[4].innerText
+
+document.children[0].children[1].children[4].children[3].style.color = "#ff0000"
+
+document.children[0].children[1].children[4].children[4].innerText = "Jamboree sandwich at the Jambo-ree"
+
+var node = document.createElement("img");                 
+var textnode = document.setAttribute("https://images.pexels.com/photos/994605/pexels-photo-994605.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500");         
+node.appendChild(textnode);                              
+
+
