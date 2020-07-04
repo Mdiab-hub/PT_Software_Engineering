861e333025fd2aca0c7b95107a96e723f16dbd37
Mdiab-hub <Mdiab-hub>
2020-07-04 19:37:53 -0400

    Commit 1 - I added my first element and it was fun

--- Mod1/week-5/w5d3/homework/hogwarts/app.js
+++ Mod1/week-5/w5d3/homework/hogwarts/app.js
@@ -1,4 +1,11 @@
-if (typeof jQuery == 'undefined'){
-    console.log('oops! I still have to link my jQuery properly!');
-  } else {console.log('I did it! I linked jQuery and this js file!')};
-  
+$(() => {
+    ///////////
+    //Year 1///
+    ///////////
+    
+    const $container = $('#container')
+    //console.log($container)
+    const $h1 = $("<h1>Hogwarts<h1>")
+    $container.append($h1)
+});
+
