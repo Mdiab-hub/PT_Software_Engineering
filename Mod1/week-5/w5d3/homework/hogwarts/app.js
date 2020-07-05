43e4b9333958fe7ec370818d2f1ad61858a218de
Mdiab-hub <Mdiab-hub>
2020-07-04 20:34:56 -0400

    Commit 3 - I added list elements. That code didn't seem very dry. I wonder if there is something I could do about it in the Hungry for More section

--- Mod1/week-5/w5d3/homework/hogwarts/app.js
+++ Mod1/week-5/w5d3/homework/hogwarts/app.js
@@ -13,13 +13,29 @@ $(() => {
     ////////////
 
     const $h2 = $("<h2>Motasem<h2>")
-    const $h3 = $("<h3>Gryffindor</h3>");
+    const $h3 = $("<h3>Gryffindor</h3>")
     const $pet_Nameh4 = $("<h4 class='cat'>Ace</h4>")
-    let $h4 = $("<h4 class='wand'>Hornbeam Wand with Dragon Heartstring Core</h4>");
+    let $h4 = $("<h4 class='wand'>Hornbeam Wand with Dragon Heartstring Core</h4>")
     $container.append($h2)
     $container.append($h3)
     $container.append($pet_Nameh4)
     $container.append($h4)
 
+    ////////////
+    //Year 3///
+    //////////
+
+    $ul.attr("storage")
+    $ul.val("trunk")
+
+    const $ul = $("<ul/>")
+    const $li1 = $("<li>Butter Beer</li>")
+    const $li2 = $("<li class='secret'>Marauder's Map</li>")
+    const $li3 = $("<li class='secret'>Invisibility Cloak</li>")
+    const $li4 = $("<li class ='secret'>Time Turner</li>")
+    const $li5 = $("<li class='toad'>leash</li>")
+    const $li6 = $("<li>Bertie Bott's Every Flavor Beans</li>")
+
+    $container.append($ul)
 });
 
