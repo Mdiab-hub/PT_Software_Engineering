4e002e11f493cdab459afbea6c96dd47ee7cd1a3
Mdiab-hub <Mdiab-hub>
2020-07-07 17:58:42 -0400

    current week HW

--- Mod1/week-5/w5d3/homework/hogwarts/app.js
+++ Mod1/week-5/w5d3/homework/hogwarts/app.js
@@ -5,17 +5,17 @@ $(() => {
 
     const $container = $('#container')
     //console.log($container)
-    const $h1 = $("<h1>Hogwarts<h1>")
+    const $h1 = $("<h1>Hogwarts</h1>")
     $container.append($h1)
 
     //////////////
     //Year 2/////
     ////////////
 
-    const $h2 = $("<h2>Motasem<h2>")
-    const $h3 = $("<h3>Gryffindor</h3>")
+    const $h2 = $("<h2>").text('Motasem')
+    const $h3 = $("<h3>").text("Gryffindor")
     const $pet_Nameh4 = $("<h4 class='cat'>Ace</h4>")
-    let $h4 = $("<h4 class='wand'>Hornbeam Wand with Dragon Heartstring Core</h4>")
+    const $h4 = $("<h4 class='wand'>Hornbeam Wand with Dragon Heartstring Core</h4>")
     $container.append($h2)
     $container.append($h3)
     $container.append($pet_Nameh4)
@@ -28,21 +28,25 @@ $(() => {
     $ul.attr("storage")
     $ul.val("trunk")
 
-    const $ul = $("<ul/>")
-    const $li1 = $("<li>Butter Beer</li>")
-    const $li2 = $("<li class='secret'>Marauder's Map</li>")
-    const $li3 = $("<li class='secret'>Invisibility Cloak</li>")
-    const $li4 = $("<li class ='secret'>Time Turner</li>")
-    const $li5 = $("<li class='toad'>leash</li>")
-    const $li6 = $("<li>Bertie Bott's Every Flavor Beans</li>")
+    
+    const $ul = $("<ul></ul>")
+    const $li = $("<li></li>")
+    //const $li1 = $("<li>Butter Beer</li>")
+    // const $li2 = $("<li class='secret'>Marauder's Map</li>")
+    // const $li3 = $("<li class='secret'>Invisibility Cloak</li>")
+    // const $li4 = $("<li class ='secret'>Time Turner</li>")
+    // const $li5 = $("<li class='toad'>leash</li>")
+    // const $li6 = $("<li>Bertie Bott's Every Flavor Beans</li>")
 
-    $container.append($ul)
-    $container.append($li1)
-    $container.append($li2)
-    $container.append($li3)
-    $container.append($li4)
-    $container.append($li5)
-    $container.append($li6)
+    
+     $container.append($ul)
+     $container.$ul.append($li)
+    // $container.append($li2)
+    // $container.append($li3)
+    // $container.append($li4)
+    // $container.append($li5)
+    // $container.append($li6)
+    // $container.append("</ul>")
 
     //////////////
     //year 4//////
@@ -51,5 +55,20 @@ $(() => {
 
     const $h5 = $("<h5>Spring 2017</h5>")
     const $table = $("<table> <tr><th>Day</th><th>Classes</th></tr> <tr><th>Monday</th><th>Transfiguration, Charms</th></tr> <tr><th>Tuesday</th><th> Divination, History of Magic</th></tr> <tr><th>Wednesday</th><th>Transfiguration, Charms</th></tr></tr> <tr><th>Thursday</th><th>Herbology, Potions</th></tr> <tr><th>Friday</th><th>Defense Against the Dark Arts</th></tr> </table>")
+
+    ///////////
+    //Year 5////
+    ///////////
+
+// $h4.remove();
+// $li1.remove();
+// $h4.text("Red Mahogany Wand with Dragon Heartstring Core")
+// $('h4').first().addClass('');
+// $h3.after($h4)
+// $('.newWand').css('color', 'maroon');
+
+// $('h6').appendTo('ul');
+// $('h6').appendTo('h4');
+
 });
 
